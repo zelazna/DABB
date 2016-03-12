275d9c69878ffe7a0bbcb7833a4aa22c4025c0b3
Constantin Guidon <constantin.guidon@gmail.com>
2016-03-12 20:25:29 +0100

    fix bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 function game() {
     $(document).ready(function () {
 
-        var realCanvas = $("#canvas");
-        var canvas = realCanvas[0];
+        var canvas = $("#canvas")[0];
         var ctx = canvas.getContext("2d");
-        var w = realCanvas.width();
-        var h = realCanvas.height();
+        var w = $("#canvas").width();
+        var h = $("#canvas").height();
         var cw = 20;
-        //var allRedArray;
+        var allRedArray;
         var red_array;
         //var red_target_array;
         var blue_array;
         //var blue_target_array;
         var points = 0;
 
+
+        create_red();
         function create_red() {
             var length = 3;
             red_array = [];
@@ -22,39 +23,37 @@ function game() {
             }
         }
 
-        create_red();
-        console.log(red_array);
-
+        create_blue();
         function create_blue() {
             var length = 1;
             blue_array = [];
             for (var i = length - 1; i >= 0; i--) {
-                blue_array.push({x: 8, y: -5});
+                blue_array.push({x: 8, y: -3});
             }
         }
 
-        create_blue();
+        console.log(blue_array);
 
         //--CRÉATION DES TARGET
 
-
-        //(function red_target() {
+        //red_target();
+        //function red_target() {
         //    var length = 1;
         //    red_target_array = [];
         //    for (var i = length - 1; i >= 0; i--) {
         //        red_target_array.push({x: 3, y: 27});
         //    }
-        //})();
+        //}
         //
         //
-        //
-        //(function blue_target() {
+        //blue_target();
+        //function blue_target() {
         //    var length = 1;
         //    blue_target_array = [];
         //    for (var i = length - 1; i >= 0; i--) {
         //        blue_target_array.push({x: 8, y: 27});
         //    }
-        //})();
+        //}
 
         function create_target(coordX, coordY) {
             var length = 1;
@@ -62,35 +61,33 @@ function game() {
             for (var i = length - 1; i >= 0; i--) {
                 target_array.push({x: coordX, y: coordY});
             }
-            console.log(target_array[0].y);
-            console.log(target_array[0].x);
             return target_array;
         }
 
         var blue_target_array = create_target(8, 27);
         var red_target_array = create_target(3, 27);
 
-
+        paint();
         function paint() {
             ctx.fillStyle = "white";
             ctx.fillRect(0, 0, w, h);
             ctx.strokeStyle = "black";
             ctx.strokeRect(0, 0, w, h);
-            //limit();
+            limit();
+
         }
 
-        paint();
-
         function paintRed() {
-            //console.log(red_array[0].y);
             var ny = red_array[0][0].y;
             ny++;
+
             var tailR = red_array[0].pop();
             tailR.y = ny;
             red_array[0].unshift(tailR);
 
             for (var i = 0; i < red_array.length; i++) {
-                var c = red_array[i];
+                var c = red_array[0][0];
+
                 ctx.fillStyle = "red";
                 ctx.fillRect(c.x * cw, c.y * cw, cw, cw);
                 ctx.strokeStyle = "white";
@@ -123,46 +120,48 @@ function game() {
 
             for (var i = 0; i < blue_target_array.length; i++) {
                 var c = blue_target_array[i];
-                ctx.fillStyle = "darkblue";
+                ctx.fillStyle = "deepblue";
                 ctx.fillRect(c.x * cw, c.y * cw, cw, cw);
                 ctx.strokeStyle = "white";
                 ctx.strokeRect(c.x * cw, c.y * cw, cw, cw);
             }
         }
 
-        //function fivePoint() {
-        //    points += 5;
-        //    console.log(points);
-        //    red_array.shift();
-        //    create_red();
-        //}
-        //
-        //function tenPoint() {
-        //    points += 10;
-        //    console.log(points);
-        //    red_array.shift();
-        //    create_red();
-        //}
-
-        function pointsUp(won_points) {
-            points += won_points;
+        function fivePoint() {
+            points += 5;
             console.log(points);
+
             red_array.shift();
+            create_red();
         }
 
-        //function limit() {
-        //    var nx = red_array[0][0].x;
-        //    var ny = red_array[0][0].y;
-        //    var ryLimit = red_target_array[0].y + 1;
-        //    if (ny == ryLimit) {
-        //        red_array.shift();
-        //    }
-        //}
+        function tenPoint() {
+            points += 10;
+            console.log(points);
+
+            red_array.shift();
+            create_red();
+        }
+
+        function limit() {
+            var nx = red_array[0].x;
+            var ny = red_array[0].y;
+
+            var ryLimit = red_target_array[0].y + 1;
+            if (ny == ryLimit) {
+                red_array.shift();
+
+            }
+
+        }
+
 
         //function song() {
         //    create_red();
         //    setTimeout(create_red, 2000);
+        //
         //}
+
 
         $(document).keydown(function (e) {
             //check event for the red
@@ -173,8 +172,8 @@ function game() {
             var red_ryMin = red_target_array[0].y - 1;
             //var ryMax = target_array[0].y + 1;
             var left_key = e.which;
-            if (left_key == "37" && (red_nx == red_rx && red_ny == red_ry)) pointsUp(10);
-            else if (left_key == "37" && (red_nx == red_rx && red_ny == red_ryMin)) pointsUp(5);
+            if (left_key == "37" && (red_nx == red_rx && red_ny == red_ry)) tenPoint();
+            else if (left_key == "37" && (red_nx == red_rx && red_ny == red_ryMin)) fivePoint();
 
             //check event for the blue
             var blue_nx = blue_array[0].x;
@@ -184,8 +183,8 @@ function game() {
             var blue_ryMin = blue_target_array[0].y - 1;
             //var ryMax = blue_target_array[0].y + 1;
             var right_key = e.which;
-            if (right_key == "39" && (blue_nx == blue_rx && blue_ny == blue_ry)) pointsUp(10);
-            else if (right_key == "39" && (blue_nx == blue_rx && blue_ny == blue_ryMin)) pointsUp(5);
+            if (right_key == "39" && (blue_nx == blue_rx && blue_ny == blue_ry)) tenPoint();
+            else if (right_key == "39" && (blue_nx == blue_rx && blue_ny == blue_ryMin)) fivePoint();
 
         });
 
@@ -193,7 +192,5 @@ function game() {
         var game_loop = setInterval(paint, globalLoop);
         var red_loop = setInterval(paintRed, globalLoop);
         var blue_loop = setInterval(paintBlue, globalLoop);
-
-
     })
 }
