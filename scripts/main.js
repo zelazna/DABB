66d1815d60e204bef86ea5ed4aa471ebb8e36585
Constantin Guidon <constantin.guidon@gmail.com>
2016-03-24 22:09:06 +0100

    clean

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,13 @@ $(function () {
 
 var red_timing = [1100, 2500, 3700, 4200, 5300, 6000];
 var blue_timing = [1900, 2200, 4000, 5200, 7300, 8000];
-//var countElements = 1;
 var game = {};
 game.score = 0;
 var count = 0;
 var $canvas = $('.canvas');
 
 // FONCTIONS
+
 
 function createRed() {
     var $cubeRed = $('<div></div>');
@@ -31,7 +31,6 @@ function createRed() {
     }, 2000, 'linear');
 
 }
-
 
 function createBlue() {
     var $cubeBlue = $('<div></div>');
@@ -49,13 +48,11 @@ function init() {
 
     for (var i = 0; i < red_timing.length; i++) {
         setTimeout(createRed, red_timing[i]);
-
     }
 
     for (var j = 0; j < blue_timing.length; j++) {
         setTimeout(createBlue, blue_timing[j]);
     }
-
 
 }
 
@@ -68,7 +65,6 @@ function init() {
 function tenPoint() {
     game.score += 10;
     $(".score").text(game.score);
-    console.log("10pts");
 }
 
 
@@ -80,15 +76,14 @@ $(document).keydown(function (e) {
     //check event for the red
     event.stopPropagation();
     if (e.which == "37") {
-        // redTargetY = 372
         $('.red').each(function () {
             var $red = $(this);
             var redPosition = $red.position();
             var redY = parseInt(redPosition.top);
             var redTargetposition = $('.redtarget').position();
             var redTargetY = parseInt(redTargetposition.top);
-            if (redY >= redTargetY - 30 && redY <= redTargetY + 30) {
-                tenPoint().delay(300);
+            if (redY >= redTargetY - 20 && redY <= redTargetY + 20) {
+                tenPoint();
                 // } else if (redY >= redTargetY - 70 && redY <= redTargetY + 70) {
                 //     fivePoint();
             }
@@ -102,7 +97,7 @@ $(document).keydown(function (e) {
             var blueY = parseInt(bluePosition.top);
             var blueTargetPosition = $('.bluetarget').position();
             var blueTargetY = parseInt(blueTargetPosition.top);
-            if (blueY >= blueTargetY - 30 && blueY <= blueTargetY + 30) {
+            if (blueY >= blueTargetY - 20 && blueY <= blueTargetY + 20) {
                 tenPoint();
                 // } else if (blueY >= blueTargetY - 70 && blueY <= blueTargetY + 70) {
                 //     fivePoint();
@@ -122,3 +117,4 @@ $(document).keydown(function (e) {
         $(".bluetarget").animate({opacity: 'toggle'}, "fast").delay(100).fadeIn(200);
     }
 });
+
